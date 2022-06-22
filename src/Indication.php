abfec65b10229ca6890d70bf87c3546b6a628d47
Maycon Oliveira <rick.maycon.98@gmail.com>
2022-06-21 21:43:32 -0300

    feat: add indication date

--- src/Indication.php
+++ src/Indication.php
@@ -6,11 +6,14 @@ class Indication
 {
     private  Student  $indicated;
     private  Student  $indicator;
+    private  \DateTimeImmutable $date;
 
     public function __construct(Student $indicated, Student $indicator)
     {
         $this->indicated = $indicated;
         $this->indicator = $indicator;
+
+        $this->date = new \DateTimeImmutable();
     }
 
 }
